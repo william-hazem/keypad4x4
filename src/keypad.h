16dbdedfe38152e976d9f39d8a4e352da446331a
William Henrique <william.martins@ee.ufcg.edu.br>
2022-08-24 10:16:29 -0300

    license apache v2

--- src/keypad.h
+++ src/keypad.h
@@ -1,3 +1,18 @@
+// Copyright 2022  William Henrique (william.martins@ee.ufcg.edu.br)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 #ifndef KEYPAD_H
 #define KEYPAD_H
 
@@ -7,8 +22,6 @@
  * @brief 4x4 Keypad library for ESP-IDF framework
  * @version 0.1
  * @date 2022-07-30
- * 
- * @copyright Copyright (c) 2022
  * 
  */
 
